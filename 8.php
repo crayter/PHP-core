22f102a6735c21c6c06736d275162c8a39d93dee
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 22:56:52 +0200

    Task 8

--- 8.php
+++ 8.php
@@ -1,1 +1,16 @@
 <?php
+
+for ($i = 0; $i < 19; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo "* ";
+    }
+    echo '<br>';
+}
+
+for ($i = 0; $i < 19; $i++) {
+    for ($j = 19; $j > $i; $j--) {
+        echo "* ";
+    }
+    echo '<br>';
+}
+
