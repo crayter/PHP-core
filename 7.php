4af0b6d82cffd9092578e492accc277f46995bf2
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 22:43:26 +0200

    Task 7

--- 7.php
+++ 7.php
@@ -1,1 +1,22 @@
-<?php
+<!DOCTYPE html>
+  <h3>Chess Board</h3>
+   <table width="400px" cellspacing="0px" cellpadding="10px" border="1px">
+      <?php
+      for($row=1;$row<=8;$row++)
+	  {
+          echo "<tr>";
+          for($col=1;$col<=8;$col++)
+		  {
+          $total=$row+$col;
+          if($total%2==0) {
+            echo "<td height=30px width=30px bgcolor=black></td>";
+          }
+		  else {
+            echo "<td height=30px width=30px bgcolor=white></td>";
+          }
+          }
+            echo "</tr>";
+    }
+          ?>
+  </table>
+
