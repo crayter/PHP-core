824f3d73f704511b62ea1d0abdca5ff1d6180e1e
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 18:36:46 +0200

    Task 2

--- 2.php
+++ 2.php
@@ -1,1 +1,16 @@
 <?php
+
+$number = 5;
+
+while ($number <= 15) {
+    echo $number;
+    $number += 1;
+    echo '<br>';
+}
+echo '<br>';
+
+for ($i = 5; $i <= 15; $i++) {
+    echo $i;
+    echo '<br>';
+}
+echo '<br>';
