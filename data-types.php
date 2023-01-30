f166cbe1bc017192e60f74c2f4d1e8f2d3d5f648
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 18:29:31 +0200

    data types exercises

---
+++ data-types.php
@@ -0,0 +1,84 @@
+<?php
+function convertToInt(string $str): int
+{
+    if ($str)
+    {
+        $int_value = (int) $str;
+        return $int_value;
+    } else {
+        return 0;
+    }
+
+}
+var_dump(convertToInt("123"));
+echo '<br>';
+
+function convertToStr(int $num): string
+{
+    if ($num)
+    {
+        $str_value = (string) $num;
+        return $str_value;
+    } else {
+        return "";
+    }
+
+}
+var_dump(convertToStr(123));
+echo '<br>';
+
+
+function convertToFloat(float $float): float
+{
+    if ($float)
+    {
+        $float_value = (float) $float;
+        return $float_value;
+    } else {
+        return 0.0;
+    }
+
+}
+var_dump(convertToFloat(123.239999));
+echo '<br>';
+
+
+function convertToBool(string $bool): bool
+{
+    if ($bool)
+    {
+        $bool = filter_var($bool, FILTER_VALIDATE_BOOLEAN);
+        return $bool;
+    } else {
+        return false;
+    }
+
+}
+var_dump(convertToBool('true'));
+echo '<br>';
+
+function convertToArray(string $arr): array
+{
+    if ($arr)
+    {
+        for ($i = 0; $i < $arr; $i++)
+        {
+            $arr = str_split($arr);
+            return $arr;
+        }
+    }
+    return [];
+}
+var_dump(convertToArray('Hello Guys!'));
+echo '<br>';
+
+
+function convertToNull(int $notNull): ?int {
+    if ($notNull < 100)
+    {
+        return $notNull;
+    }
+    return null;
+}
+var_dump(convertToNull(200));
+echo '<br>';
