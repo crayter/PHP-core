c6d2c3b23f4d7bba9a288626d8b282d6c56a74ba
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 11:05:33 +0200

    Task 15

--- 15.php
+++ 15.php
@@ -1,1 +1,16 @@
 <?php
+
+function whatKindOf($number) {
+    $zero = 0;
+    if ($number > $zero && is_numeric($number)) {
+        echo "$number is a positive number";
+    } elseif ($number < $zero && is_numeric($number)) {
+        echo "$number is a negative number";
+    } elseif (is_numeric($number)) {
+        echo "The number is $number";
+    } else {
+        echo "$number - is a string.";
+    }
+}
+
+echo whatKindOf("0");
