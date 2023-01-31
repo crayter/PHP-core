f6a219fbf8695a25082af52b5315d2ee8dea422e
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 10:23:49 +0200

    Task 13

--- 13.php
+++ 13.php
@@ -1,1 +1,10 @@
 <?php
+
+function rect_area(int $length,int $width) 
+{
+    $area = $length * $width;
+    echo "Area Of Rectangle with length " . $length . " & width " . $width . " = " . $area;
+}
+
+rect_area(22, 10);
+
