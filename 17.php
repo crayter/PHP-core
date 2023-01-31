6b3751911133c1dc755b61655c31c15f9df162eb
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 12:43:21 +0200

    Task 17,18

--- 17.php
+++ 17.php
@@ -1,1 +1,5 @@
 <?php
+
+$string = "What is your name?";
+
+echo "This string has" . " " . strlen($string) . " " . "characters" ;
--- 18.php
+++ 18.php
@@ -1,1 +1,5 @@
 <?php
+
+$string = "There are not many words";
+
+echo "This string contains" . " " . str_word_count($string) . " " . "words.";
