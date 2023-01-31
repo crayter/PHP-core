5ba61be176d36a398d13094f3c82f961190e4ef3
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 10:12:46 +0200

    Task 11

--- 11.php
+++ 11.php
@@ -1,1 +1,25 @@
 <?php
+function calculate($num1,$num2,$op)
+{
+$calc = 0;
+
+switch ($op)
+{
+    case '+':
+        $calc = $num1 + $num2;
+        break;
+    case '-':
+        $calc = $num1 - $num2;
+        break;
+    case '*':
+        $calc = $num1 * $num2;
+        break;
+    case '/':
+        $calc = $num1 / $num2;
+        break;
+}
+
+    return $calc;
+
+}
+echo calculate(6, 2, '+');
