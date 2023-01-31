8040182b1b00c30f2d4ea009f96793a2938bad1f
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 10:18:15 +0200

    Task 12

--- 12.php
+++ 12.php
@@ -1,1 +1,10 @@
 <?php
+
+function canVote($age): string {
+    if ($age < 18) {
+        return "{$age} years old - not allowed to vote.";
+    }
+    return "{$age} years old - eligible to vote.";
+}
+
+echo canVote(17);
