39345f43961b2cbd2bb25054d664fffd26125f46
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 12:56:14 +0200

    Task 20

--- 20.php
+++ 20.php
@@ -1,1 +1,19 @@
-<?php
+<title>
+Array to String
+</title>
+</head>
+<body>
+	<form action="data.php" method="post">
+    Username: <input type="text" name="username" placeholder="your name" required/><br/><br/>
+		What's your favorite color?<br/>
+		Red<input type="checkbox" name="check_list[]" value="red"/><br/>
+		Blue<input type="checkbox" name="check_list[]" value="blue"/><br/>
+		Green<input type="checkbox" name="check_list[]" value="green"/><br/>
+		Yellow<input type="checkbox" name="check_list[]" value="yellow"/><br/>
+		Pink<input type="checkbox" name="check_list[]" value="pink"/><br/>
+		Black<input type="checkbox" name="check_list[]" value="black"/><br/>
+		White<input type="checkbox" name="check_list[]" value="white"/><br/><br/>
+		<input type="submit" name="submit" value="Submit"/><br/>
+	</form>
+
+
---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['check_list'])) {
+        // Counting number of checked checkboxes.
+        $checked_count = count($_POST['check_list']);
+        $name = $_POST['username'];
+        if ($checked_count < 2) {
+            echo $name . " 's favourite colors is " . $checked_count . "<br/>";
+            foreach ($_POST['check_list'] as $selected) {
+                echo "<p>" . $selected . "</p>";
+            }
+        } else {
+            echo $name . " 's favourite colors are " . $checked_count . "<br/>";
+            foreach ($_POST['check_list'] as $selected) {
+                echo "<p>" . $selected . "</p>";
+            }
+        }
+    } else {
+        echo "<b>Please Select Atleast One Option.</b>";
+    }
+}
