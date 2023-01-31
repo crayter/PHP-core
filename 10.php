844e8918c4ebbddd1cfc8437fdc2823c90011cc2
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 10:01:54 +0200

    Task 10

--- 10.php
+++ 10.php
@@ -1,1 +1,23 @@
 <?php
+echo "<table border =\"1\" style='border-collapse: collapse'>";
+
+echo "<tr style='font-weight: bold;'>";
+echo "<td > </td> \n";
+for ($col=1; $col <= 12; $col++) {
+    echo "<td>$col</td> \n";
+}
+echo "</tr>";
+
+for ($row=1; $row <= 12; $row++) {
+    echo "<tr> \n";
+
+    echo "<td style='font-weight: bold;'>$row</td>";
+
+    for ($col=1; $col <= 12; $col++) {
+        $p = $col * $row;
+        echo "<td>$p</td> \n";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
