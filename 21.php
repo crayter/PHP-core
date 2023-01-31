3d772b3900006b4e8145333d491cca83175f1c23
crayter <97885267+crayter@users.noreply.github.com>
2023-01-31 13:15:09 +0200

    Task 21

--- 21.php
+++ 21.php
@@ -1,1 +1,11 @@
 <?php
+
+$isSubset1 = ['Apple','Strawberry','Raspberry','20','3', 'Banana'];
+$isSubset2 = ['20','Apple'];
+
+
+if (array_intersect($isSubset2, $isSubset1) === $isSubset2) {
+    echo "It's a subset";
+} else {
+    echo "It's not a subset";
+}
